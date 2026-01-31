1e9d5cbf1b28fba216d64e9cd563acf3ca5e9436
ch7oe <chloenixon723@gmail.com>
2026-01-30 20:49:40 -0500

    add runner for enhanced currency agent

--- day_2/agent_tools/run.py
+++ day_2/agent_tools/run.py
@@ -1,5 +1,5 @@
 import asyncio
-from .agent import currency_agent
+from .agent import currency_agent, enhanced_currency_agent
 from utils.helpers import get_api_key
 from google.adk.runners import InMemoryRunner
 
@@ -8,8 +8,11 @@ async def main(query: str):
 
     get_api_key()
 
-    currency_runner = InMemoryRunner(agent=currency_agent)
-    response = await currency_runner.run_debug(query)
+    # currency_runner = InMemoryRunner(agent=currency_agent)
+    # response = await currency_runner.run_debug(query)
+
+    enhanced_runner = InMemoryRunner(agent=enhanced_currency_agent)
+    response = await enhanced_runner.run_debug(query)
 
     print("---EVENTS---\n")
     for event in response:
@@ -17,5 +20,5 @@ async def main(query: str):
 
 
 if __name__ == "__main__":
-    user_query = "I want to convert 500 US Dollars to Euros using my Platinum Credit Card. How much will I receive?"
+    user_query = "Convert 1,250 USD to INR using a Bank Transfer. Show me the precise calculation."
     asyncio.run(main(user_query))
