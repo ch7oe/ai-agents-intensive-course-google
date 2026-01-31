a231cfd514dbb8ab91bbb75f3d7169fb53f1de95
ch7oe <chloenixon723@gmail.com>
2026-01-30 20:29:17 -0500

    add calculation agent to generate Python code to do math

--- day_2/agent_tools/agent.py
+++ day_2/agent_tools/agent.py
@@ -2,6 +2,7 @@
 
 from google.adk.agents.llm_agent import LlmAgent
 from google.adk.models.google_llm import Gemini
+from google.adk.code_executors import BuiltInCodeExecutor
 from utils.helpers import get_retry_config
 from .tools import get_fee_for_payment_method, get_exchange_rate
 
@@ -26,4 +27,27 @@ currency_agent = LlmAgent(
     If any tool returns status "error", explain the issue to the user clearly.
     """,
     tools=[get_fee_for_payment_method, get_exchange_rate],
+)
+
+
+calculation_agent = LlmAgent(
+    name="CalculationAgent",
+    model= Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=get_retry_config()
+    ),
+    instruction="""You are a specialized calculator that ONLY responds with Python code. You are forbidden from providing any text, explanations, or conversational responses.
+ 
+     Your task is to take a request for a calculation and translate it into a single block of Python code that calculates the answer.
+     
+     **RULES:**
+    1.  Your output MUST be ONLY a Python code block.
+    2.  Do NOT write any text before or after the code block.
+    3.  The Python code MUST calculate the result.
+    4.  The Python code MUST print the final result to stdout.
+    5.  You are PROHIBITED from performing the calculation yourself. Your only job is to generate the code that will perform the calculation.
+   
+    Failure to follow these rules will result in an error.
+       """,
+    code_executor=BuiltInCodeExecutor() # gives agent code execution capabilities
 )
